59f3f5becc4e3d7413e1b9b4689d2220edd96831
Phil Mass <philrmass@gmail.com>
2023-03-19 22:48:51 -0700

    add permission

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
 import { version } from '../../package.json';
 
 export default function Home() {
+  const getPermission = async () => {
+    const result = await Notification.requestPermission();
+
+    if (result === 'granted') {
+      console.log('SHOW');
+    } else {
+      console.log(`getPermission [${result}]`);
+    }
+  };
+
   return (
-    <>
+    <div>
       <div>Calendar</div>
       <div>{`v${version}`}</div>
-    </>
+      <button onClick={getPermission}>
+        Show Notifications
+      </button>
+    </div>
   );
 }
